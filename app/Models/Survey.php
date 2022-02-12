afce28def5c9ec76b1fd8d8b7572c69fdb511a62
Eduardo Guastay <compunethomemx@gmail.com>
2022-02-12 00:36:16 -0500

    Survey model ready

---
+++ app/Models/Survey.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Survey extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey ='cod_survey';
+
+    protected $fillable=[
+        'cod_survey',
+        'name',
+        'date_init',
+        'date_finally',
+        'status',
+        'detail',
+        'cod_project'
+    ];
+
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'date_init' => 'datetime',
+        'date_finally' => 'datetime',
+    ];
+
+    public function project(){
+        return $this->belongsTo(Project::class,'cod_project');
+    }
+
+    public function questions(){
+        return $this->hasMany(Question::class,'cod_survey');
+    }
+}
