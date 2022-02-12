d0988178da958958a789193b95b7bc1ef49256e9
Eduardo Guastay <compunethomemx@gmail.com>
2022-02-12 00:36:04 -0500

    Role model ready

---
+++ app/Models/Role.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey="cod_rol";
+
+    protected $fillable=[
+        "name",
+        "detail"
+    ];
+
+    public function users(){
+        return $this->hasMany(User::class,'id');
+    }
+
+    public function access(){
+        return $this->belongsToMany(Access::class,'access_role','cod_rol','cod_access');
+    }
+}
