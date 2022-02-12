de583469f9f038434d62b15dd086789efa3ca7ea
Eduardo Guastay <compunethomemx@gmail.com>
2022-02-12 00:34:44 -0500

    Access model ready

---
+++ app/Models/Access.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Access extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey='cod_access';
+    protected $table='access';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable=[
+        "name",
+        "endpoint",
+        "method",
+        "detail"
+    ];
+
+    protected $hidden=[
+        "created_at",
+        "updated_at"
+    ];
+
+    /**
+     *returns the menu to which the access belongs
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function menu(){
+        return $this->belongsTo(Menu::class,'cod_menu');
+    }
+
+    /**
+     *
+     * returns the roles in which the access is found
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles(){
+        return $this->belongsToMany(Role::class,'access_role','cod_access','cod_rol');
+    }
+}
