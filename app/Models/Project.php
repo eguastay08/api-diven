58d7b27047d4e2d189f70a94f0cdbc2b47a9ed1c
Eduardo Guastay <compunethomemx@gmail.com>
2022-02-12 00:35:44 -0500

    Project model ready

---
+++ app/Models/Project.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey='cod_project';
+
+    protected $fillable=[
+        'name',
+        'resolution',
+        'detail',
+        'image',
+        'cod_dpa',
+        'create_at',
+        'update_at'
+    ];
+
+    public function dpa(){
+        return $this->belongsTo(Dpa::class,'cod_dpa');
+    }
+
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
+
+    public function surveys(){
+        return $this->hasMany(Survey::class,'cod_project');
+    }
+}
